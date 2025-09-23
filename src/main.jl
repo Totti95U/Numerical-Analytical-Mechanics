cc45cd252b4355fef089813489b430a0c047d32a
Totti95U <u.mitomayu984@gmail.com>
2025-09-24 00:52:46 +0900

    bug: fix type of `q_to_x` field in `Lagrangian` #2

--- src/main.jl
+++ src/main.jl
@@ -12,7 +12,7 @@ struct Lagrangian
     q::Symbolics.Arr{Num, 1}
     q_dot::Symbolics.Arr{Num, 1}
     L::Num
-    q_to_x::Dict{Num, Num}  # 一般化座標から直交座標への変換辞書
+    q_to_x::Vector{Num} # 一般化座標から直交座標への変換関数を格納するベクトル
 
     function Lagrangian(t::Num, q::Symbolics.Arr{Num, 1}, q_dot::Symbolics.Arr{Num, 1}, L::Num, q_to_x::Vector{Num}=Vector{Num}())
         new(t, q, q_dot, L, q_to_x)
