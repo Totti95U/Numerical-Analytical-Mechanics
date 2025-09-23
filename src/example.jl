5d3e88db3a81e888811b04ab3e2257210f2f0962
Totti95U <u.mitomayu984@gmail.com>
2025-09-24 00:44:16 +0900

    refactor: `q_to_x` の扱いを数式のベクトル変更 #2

--- src/example.jl
+++ src/example.jl
@@ -9,11 +9,12 @@ q_dot = D.(q)
 @parameters m, r
 
 @variables (x(t))[1:4]
-q_to_x = Dict{Num, Num}()
-q_to_x[x[1]] = sin(q[1])
-q_to_x[x[2]] = cos(q[1])
-q_to_x[x[3]] = sin(q[1]) + r * sin(q[2])
-q_to_x[x[4]] = cos(q[1]) + r * cos(q[2])
+q_to_x = [
+    sin(q[1]),
+    cos(q[1]),
+    sin(q[1]) + r * sin(q[2]),
+    cos(q[1]) + r * cos(q[2]),
+]
 
 T = (1//2) * q_dot[1]^2 + (1//2) * m * (q_dot[1]^2 + r^2 * q_dot[2]^2 + 2 * r * cos(q[1] - q[2]) * q_dot[1] * q_dot[2])
 V = -9.8 * cos(q[1]) - m * 9.8 * (cos(q[1]) + r * cos(q[2]))
--- src/main.jl
+++ src/main.jl
@@ -3,7 +3,7 @@
 using ModelingToolkit
 
 """
-    Lagrangian(t, q, q_dot, L, q_to_x=Dict{Num, Num}())
+    Lagrangian(t, q, q_dot, L, q_to_x=Vector{Num}())
 S
 ラグランジアンを表す構造体。
 """
@@ -14,7 +14,7 @@ struct Lagrangian
     L::Num
     q_to_x::Dict{Num, Num}  # 一般化座標から直交座標への変換辞書
 
-    function Lagrangian(t::Num, q::Symbolics.Arr{Num, 1}, q_dot::Symbolics.Arr{Num, 1}, L::Num, q_to_x::Dict{Num, Num}=Dict{Num, Num}())
+    function Lagrangian(t::Num, q::Symbolics.Arr{Num, 1}, q_dot::Symbolics.Arr{Num, 1}, L::Num, q_to_x::Vector{Num}=Vector{Num}())
         new(t, q, q_dot, L, q_to_x)
     end
 end
