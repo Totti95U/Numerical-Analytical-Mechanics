c295bd26419ebc39e7fc7c939d4e704b8629de79
Totti95U <u.mitomayu984@gmail.com>
2025-09-24 00:24:58 +0900

    bug: fix kinetic energy term

--- src/example.jl
+++ src/example.jl
@@ -15,7 +15,7 @@ q_to_x[x[2]] = cos(q[1])
 q_to_x[x[3]] = sin(q[1]) + r * sin(q[2])
 q_to_x[x[4]] = cos(q[1]) + r * cos(q[2])
 
-T = (1//2) * q_dot[1]^2 + (1//2) * m * (q_dot[1]^2 + r^2 * q_dot[2]^2 + 2 * r * cos(q[1] * q[2]) * q_dot[1] * q_dot[2])
+T = (1//2) * q_dot[1]^2 + (1//2) * m * (q_dot[1]^2 + r^2 * q_dot[2]^2 + 2 * r * cos(q[1] - q[2]) * q_dot[1] * q_dot[2])
 V = -9.8 * cos(q[1]) - m * 9.8 * (cos(q[1]) + r * cos(q[2]))
 Lexp = T - V
 lag = Lagrangian(t, q, q_dot, Lexp, q_to_x)
