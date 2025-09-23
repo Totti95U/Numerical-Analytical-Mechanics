1ae1937e7f58049ef97cdca08b4685b959256b97
Totti95U <u.mitomayu984@gmail.com>
2025-09-24 00:24:13 +0900

    bug: put `q_to_x` in `Lagrangian` constructor

--- src/main.jl
+++ src/main.jl
@@ -15,6 +15,6 @@ struct Lagrangian
     q_to_x::Dict{Num, Num}  # 一般化座標から直交座標への変換辞書
 
     function Lagrangian(t::Num, q::Symbolics.Arr{Num, 1}, q_dot::Symbolics.Arr{Num, 1}, L::Num, q_to_x::Dict{Num, Num}=Dict{Num, Num}())
-        new(t, q, q_dot, L)
+        new(t, q, q_dot, L, q_to_x)
     end
 end
