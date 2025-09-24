509a7f2fcb48feab6f24ddbda7b5f7e7fa92b273
Totti95U <u.mitomayu984@gmail.com>
2025-09-24 23:09:59 +0900

    feat: add single pendulum example

---
+++ example/pendulum.jl
@@ -0,0 +1,24 @@
+# using Latexify
+
+include("../src/main.jl")
+
+@variables t
+n = 1
+@variables (q(t))[1:n]
+D = Differential(t)
+q_dot = D.(q)
+
+@parameters g
+
+@variables (x(t))[1:2]
+q_to_x = [
+    sin(q[1]),
+    cos(q[1]),
+]
+
+T = (1//2) * q_dot[1]^2
+V = -g * cos(q[1])
+Lexp = T - V # |> expand_derivatives |> simplify
+lag = Lagrangian(t, q, q_dot, Lexp, q_to_x);
+
+ham = hamiltonian(lag);
