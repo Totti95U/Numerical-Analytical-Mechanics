bd5e27577f10d00e6690cc74ab86cb00800e9fcb
Totti95U <u.mitomayu984@gmail.com>
2025-09-29 23:56:49 +0900

    feature: add `solve` function

--- src/main.jl
+++ src/main.jl
@@ -78,7 +78,7 @@ end
 
 Solve the Hamiltonian system using leapfrog integration.
 """
-function solve(ham::Hamiltonian, qp0::Vector{Float64}, tspan::Tuple{Float64, Float64}; pram::Dict{Num,Float64}, dt=1e-3)
+function solve(ham::Hamiltonian, qp0::Vector{Float64}, tspan::Tuple{Float64, Float64}; param::Dict{Num,Float64}, dt=1e-3)
     t = ham.t
     q = ham.q
     p = ham.p
@@ -88,7 +88,7 @@ function solve(ham::Hamiltonian, qp0::Vector{Float64}, tspan::Tuple{Float64, Flo
     @variables (p_(t))[1:n]
 
     # substitute parameters
-    H = substitute(H, pram) |> simplify
+    H = substitute(H, param) |> simplify
 
     # construct the hamiltonian system
     dHdq = [expand_derivatives(derivative(H, q[i])) |> simplify for i in 1:n]
@@ -129,4 +129,53 @@ function solve(ham::Hamiltonian, qp0::Vector{Float64}, tspan::Tuple{Float64, Flo
     return ts, qs, ps
 end
 
+function in_cartesian(system::Union{Lagrangian,Hamiltonian}, q::Vector{Float64}; param=Dict{Num,Float64}())
+    q_to_x = substitute(system.q_to_x, param)
+    if isempty(q_to_x)
+        error("q_to_x is not defined in the system.")
+    end
+    n = length(system.q)
+    if length(q) != n
+        error("Length of q does not match the number of generalized coordinates.")
+    end
+    q_to_x_funcs = [build_function(f, system.q...; expression=Val{false}) for f in q_to_x]
+    x = [f(q...) for f in q_to_x_funcs]
 
+    return x
+end
+
+function in_cartesian(system::Lagrangian, q::Vector{Float64}, q_dot::Vector{Float64}; param=Dict{Num,Float64}())
+    x = in_cartesian(system, q, param=param)
+
+    # transform q_dot to x_dot
+    if length(q_dot) != n
+        error("Length of q_dot does not match the number of generalized coordinates.")
+    end
+    J_expr = [expand_derivatives(derivative(xi, system.q[j])) |> simplify for xi in system.q_to_x, j in 1:n]
+    J_expr = [substitute(J_expr[i, j], param) for i in axes(J_expr, 1), j in axes(J_expr, 2)]
+    J_funcs = [build_function(J_expr[i, j], system.q...; expression=Val{false}) for i in 1:length(system.q_to_x), j in 1:n]
+    J = [f(q...) for f in J_funcs]
+    x_dot = J * collect(q_dot)
+
+    return [x..., x_dot...]
+end
+
+function in_cartesian(system::Hamiltonian, q::Vector{Float64}, p::Vector{Float64}; param=Dict{Num,Float64}())
+    x = in_cartesian(system, q, param=param)
+
+    # transform p to q_dot
+    if length(p) != n
+        error("Length of p does not match the number of generalized coordinates.")
+    end
+    q_dot_funcs = [build_function(system.p_to_qdot[i], system.q..., system.p...; expression=Val{false}) for i in 1:length(system.p_to_qdot)]
+    q_dot = [f(q..., p...) for f in q_dot_funcs]
+
+    # transform q_dot to x_dot
+    J_expr = [expand_derivatives(derivative(xi, system.q[j])) |> simplify for xi in system.q_to_x, j in 1:n]
+    J_expr = [substitute(J_expr[i, j], param) for i in axes(J_expr, 1), j in axes(J_expr, 2)]
+    J_funcs = [build_function(J_expr[i, j], system.q...; expression=Val{false}) for i in 1:length(system.q_to_x), j in 1:n]
+    J = [f(q...) for f in J_funcs]
+    x_dot = J * collect(q_dot)
+
+    return [x..., x_dot...]
+end
